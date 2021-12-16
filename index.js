385298096d4049815088edc002e3051cce480c57
Neha <nehaprajapati1150@gmail.com>
2021-12-16 20:54:12 +0530

    middlewares

---
+++ index.js
@@ -0,0 +1,32 @@
+const e = require('express');
+const express = require('express')
+const bodyParser=require('body-parser')  //midleware library
+
+
+ //app is a object that describes all the web functions a server can do
+ const app=express();
+ app.get('/',(req,res)=>{
+     res.send(`
+     <div>
+     <form method="POST">
+     <input name="email" type="email" placeholder="email">
+     <input name="password" type="password" placeholder="password">
+     <input name="confirmPassword" type="password" placeholder="confirm password">
+     <button>Signup</button>
+ </form>
+     </div>
+     `)
+ });
+
+ //data need to be parsed
+ 
+//urlencoded-function within the library bodyparser which 
+//use for handling specific info that is coming as htnl form
+ app.post('/',bodyParser.urlencoded({extended:true}),(req,res)=>{
+ console.log(req.body);
+   res.send('Account Created')
+ });
+
+ app.listen(3000,()=>{
+     console.log('Listening on port 3000');
+ });

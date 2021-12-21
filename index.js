034e1f4e72b44e972de1976fc161e0e5ca6c1475
Neha <nehaprajapati1150@gmail.com>
2021-12-21 21:59:53 +0530

    shopping cart

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieSession=require('cookie-session')
 const authRouter=require('./routes/admin/auth')
 const adminProductsRouter=require('./routes/admin/products')
 const productsRouter=require('./routes/products')
+const cartsRouter=require('./routes/carts')
 
  //app is a object that describes all the web functions a server can do
  const app=express();
@@ -21,6 +22,7 @@ const productsRouter=require('./routes/products')
  app.use(authRouter)
  app.use(adminProductsRouter)
  app.use(productsRouter)
+ app.use(cartsRouter)
  
 
  app.listen(3000,()=>{
---
+++ routes/carts.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const router=express.Router();
+
+//receive a post req to add an item to a cart
+
+//receive a get req to show all items in cart
+
+//receive a post req to delete a item from a cart
+
+
+module.exports=router;

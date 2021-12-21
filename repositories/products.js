67b1b5fb4102335f7a2362a060ad392099147618
Neha <nehaprajapati1150@gmail.com>
2021-12-21 20:40:30 +0530

    seed data

--- repositories/products.js
+++ repositories/products.js
@@ -2,4 +2,4 @@ const Repository = require('./repository');
 
 class ProductsRepository extends Repository {}
 
-module.exports = new ProductsRepository('product.json');
+module.exports = new ProductsRepository('products.json');

b6098344831a426add80dfbf107e3bb1190dd6d9
Neha <nehaprajapati1150@gmail.com>
2021-12-21 23:55:28 +0530

    adding items to cart

--- routes/carts.js
+++ routes/carts.js
@@ -17,9 +17,23 @@ router.post('/cart/products',async(req,res)=>{
      cart=await cartsRepo.getOne(req.session.cartId);   //will generate card id and array of items
 
  }
-console.log(cart);
+
+ const existingItem=cart.items.find(item=>item.id===req.body.productId);
+ if(existingItem){
+     //increment quanitity and save cart
+     existingItem.quantity++;
+ }else{
+     //add new product id to items array
+     cart.items.push({id:req.body.productId,quantity:1})
+ }
+
+ await cartsRepo.update(cart.id,{
+     items:cart.items
+ });
 
     //either increment quantity for existing product
+    
+    
     //or add new product to items array 
     
 

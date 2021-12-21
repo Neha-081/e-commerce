c4a60132db870e2c9b8253ef17d9c6c4154d82ef
Neha <nehaprajapati1150@gmail.com>
2021-12-21 22:10:05 +0530

    shopping cart

--- routes/carts.js
+++ routes/carts.js
@@ -2,6 +2,11 @@ const express=require('express');
 const router=express.Router();
 
 //receive a post req to add an item to a cart
+//getting product id by clicking on add to cart
+router.post('/cart/products',async(req,res)=>{
+    console.log(req.body.productId);
+    res.send("product id")
+})
 
 //receive a get req to show all items in cart
 
